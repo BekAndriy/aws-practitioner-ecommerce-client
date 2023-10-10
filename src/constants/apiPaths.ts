6b74f40b39de82afe8aff26cfce60de44c013a4e
Andriy <andriybek@gmail.com>
2023-10-10 10:53:42 +0300

    task-4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
+  cart: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
