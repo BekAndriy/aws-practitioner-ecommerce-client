6c48cb44f4f3b60f0f72250728e5d136a85140d4
Andriy <andriybek@gmail.com>
2023-10-12 00:07:49 +0300

    Module 5. connected csv upload

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://1ce3usp1vk.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
 };
