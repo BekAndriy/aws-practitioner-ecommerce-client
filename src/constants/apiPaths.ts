a895ccfbddb88a3643d87f3f5f83b3c6c21bb05b
Andriy <andriybek@gmail.com>
2023-11-07 23:01:49 +0200

    module-9 containerization

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,16 @@
 const API_PATHS = {
   product: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "https://799cqkbcm0.execute-api.eu-central-1.amazonaws.com/dev",
+  // module-8 apigateway
+  // order: "https://799cqkbcm0.execute-api.eu-central-1.amazonaws.com/dev/api",
+  // module-9 elasticbeanstalk
+  order:
+    "http://bekandriy-cart-api-prod.eba-xu52j2kh.eu-central-1.elasticbeanstalk.com/api",
   import: "https://1ce3usp1vk.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://oqczke881c.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://799cqkbcm0.execute-api.eu-central-1.amazonaws.com/dev",
+  // module-9 elasticbeanstalk
+  cart: "http://bekandriy-cart-api-prod.eba-xu52j2kh.eu-central-1.elasticbeanstalk.com/api",
+  // module-8 apigateway
+  // cart: "https://799cqkbcm0.execute-api.eu-central-1.amazonaws.com/dev/api",
   cognito: "https://phone-for-you-dev-aaa.auth.eu-central-1.amazoncognito.com",
 };
 
